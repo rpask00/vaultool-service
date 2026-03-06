27810113f4904f7bc9a0476b79b6dd9dde194f52
Rafal Pasko <rp.261000@gmail.com>
2026-03-06 21:42:56 +0100

    fix prefix

--- src/utils/constant.rs
+++ src/utils/constant.rs
@@ -6,7 +6,7 @@ pub mod prod {
 
 pub mod test {
     use std::time::Duration;
-    pub const TEST_DB_PREFIX: &str = "test_";
+    pub const TEST_DB_PREFIX: &str = "testDB_";
     pub const APP_ADDRESS: &str = "127.0.0.1:0";
     pub const TIMEOUT: Duration = Duration::from_millis(200);
 }
