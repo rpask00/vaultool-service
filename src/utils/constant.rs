64fd490f0d0ae5f5614be4212448df2d01c44171
Rafal Pasko <rp.261000@gmail.com>
2026-03-06 21:40:38 +0100

    test variables mod.

--- src/utils/constant.rs
+++ src/utils/constant.rs
@@ -3,3 +3,10 @@ pub mod prod {
     pub const APP_ADDRESS: &str = "0.0.0.0:3000";
     pub const TIMEOUT: Duration = Duration::from_millis(200);
 }
+
+pub mod test {
+    use std::time::Duration;
+    pub const TEST_DB_PREFIX: &str = "test_";
+    pub const APP_ADDRESS: &str = "127.0.0.1:0";
+    pub const TIMEOUT: Duration = Duration::from_millis(200);
+}
